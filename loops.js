6756b6087a78f4b5833dbf5364c88593226360fc
Micah Lynn <micah@sabado.com>
2020-01-29 15:23:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
-  for (i=0; i<26; i++) {
-    console.log (`I am ${i} strange loop${i===0 ? '' : 's'}.`)
+  for (i=1; i<26; i++) {
+    console.log (`I am ${i} strange loop${i===1 ? '' : 's'}.`)
   }
 }
 
