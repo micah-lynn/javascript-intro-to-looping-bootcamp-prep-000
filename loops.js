9f2e07d868c1a9754742f185afce50c8a6b78be7
Micah Lynn <micah@sabado.com>
2020-01-29 15:11:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,5 +23,9 @@ function doWhileLoop(num) {
   function incrementVariable() {
     i = i + 1;
     return i;
-}
+  }
+  
+  do {
+    console.log ("I run once regardless.");
+  } while (incrementVariable() < 2);
 }
